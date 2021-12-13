ec3cfc469f816f60343fc5e8cc0f940ab14080de
Asma Bouhlel boughdira <abouhlel@c2r5p3.42nice.fr>
2021-12-13 14:27:59 +0100

    no libft authorized

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: abouhlel <abouhlel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 13:52:51 by abouhlel          #+#    #+#             */
-/*   Updated: 2021/12/13 14:01:25 by abouhlel         ###   ########.fr       */
+/*   Updated: 2021/12/13 14:26:07 by abouhlel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,20 @@
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
+#include <sys/time.h>
+#include <pthread.h>
 
+
+#typedef struct s_data_t
+{
+	int	philo_nb;
+	int eat_time;
+	int think_time;
+	int	sleep_time;
+	int	death_time;
+	int	eat_nb;
+	int	forks_nb;
+}				t_data;
 //=============================================================================//
 //*********************** F U N C T I O N S ***********************************//	
 //=============================================================================//
